1e65a1133ec2e86ef1d54946932ee6bbe5e7610a
Pallpop <zhufeng0543@gmail.com>
2021-04-05 23:50:04 +0800

    2021/4/5

---
+++ myblog/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+
+from .models import Post, Category, Comment
+
+choices = Category.objects.all().values_list('name', 'name')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'snippet', 'header_image', 'author', 'category', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type your title here'}),
+            'snippet': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'xxxa', 'type': 'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}, ),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+
+        }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'snippet', 'header_image', 'category', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type your title here'}),
+            'snippet': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'body')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'xxxa', 'type': 'hidden'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
